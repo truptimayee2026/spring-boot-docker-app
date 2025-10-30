c3cb38481a137ef47d7a279b70669926de63bd3a
Trupti <truptimayee2026@gmail.com>
2025-10-31 00:18:49 +0530

    added

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -36,6 +36,7 @@ public class WelcomeRestController {
 		System.out.println("New code added here");
 		logger.info("***** greetMsg() execution end *****");
 		int i = 10;
+		int i3 = 500;
 		logger.info("***** greetMsg() execution successfull *****");
 		boolean status = false;
 		logger.info("***** ***************************** *****");
