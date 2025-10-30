6e2c6652bfe1696fa088d2a4d9a556e864434174
Trupti <truptimayee2026@gmail.com>
2025-10-31 00:01:05 +0530

    modified same line as another dev has made

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -20,6 +20,7 @@ public class WelcomeRestController {
 		logger.info("***** welcomeMsg() execution start *****");
 		String msg = "Welcome to Ashok IT - Software Training Institute (HYD) ..!!";
 		logger.info("***** welcomeMsg() execution end *****");
+		String slf = "Hellow there is:";
 		String s = "hello";
 		logger.info("******************************************************************");
 		return msg;
